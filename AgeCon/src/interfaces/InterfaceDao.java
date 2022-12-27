48de7138d948701e6244ea75150f56db9ba37026
Fabricio C Santos <fsantos.veredinha@gmail.com>
2022-12-27 13:38:54 -0300

    Correção
    Correção de localização da interface Dao em outro pacote

--- AgeCon/src/interfaces/InterfaceDao.java
+++ AgeCon/src/interfaces/InterfaceDao.java
@@ -1,13 +1,15 @@
-package dao;
+package interfaces;
 
 import java.sql.SQLException;
 import javax.swing.JComboBox;
 
+
 public interface InterfaceDao {
-    
+  
     public void salvarDao(Object...valor);
     public void excluirDao(int id);
     public void consultarDao(Object...valor) throws SQLException;
     public void carregarComboBoxDao(JComboBox itens) throws SQLException;
     
 }
+
