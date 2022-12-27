04bafb2520754ae49ac54098758735f35c606399
Fabricio C Santos <fsantos.veredinha@gmail.com>
2022-12-27 13:26:12 -0300

    Atualização TipoContatoControle
    Controle Enviando os dados para a Classe Modelo Armazenar

--- AgeCon/src/controle/TipoContatoControle.java
+++ AgeCon/src/controle/TipoContatoControle.java
@@ -1,12 +1,19 @@
 package controle;
 
 import interfaces.InterfaceControle;
+import modelo.TipoContatoModelo;
 
 
 public class TipoContatoControle implements InterfaceControle {
+    
+    TipoContatoModelo tcm = new TipoContatoModelo();
 
     @Override
     public void salvarControle(Object... valor) {
+        tcm.setId((int)valor[0]);
+        tcm.setDescricao((String) valor[1]);
+        
+        //Enviar as informações para o DAO
         
     }
 
