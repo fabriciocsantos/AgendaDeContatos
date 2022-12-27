c77ba0d8fac9b732b588e398fd1e33774bed427f
Fabricio C Santos <fsantos.veredinha@gmail.com>
2022-12-27 13:31:47 -0300

    Interface DAO
    Criação da Interface DAO

---
+++ AgeCon/src/dao/InterfaceDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.sql.SQLException;
+import javax.swing.JComboBox;
+
+public interface InterfaceDao {
+    
+    public void salvarDao(Object...valor);
+    public void excluirDao(int id);
+    public void consultarDao(Object...valor) throws SQLException;
+    public void carregarComboBoxDao(JComboBox itens) throws SQLException;
+    
+}
