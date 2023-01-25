bf3fd8a478847f93caed8384108ac5276608cb1a
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-01-25 13:17:40 -0300

    Mudanças 2.0

--- AgeCon/src/util/PosicaoFormulario.java
+++ AgeCon/src/util/PosicaoFormulario.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
-/**
- *
- * @author Fabrício
- */
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+
 public class PosicaoFormulario {
+
+    //--- Abrir o formulário centralizado na tela
+
+    public void abrirFormulario(JInternalFrame janela, JDesktopPane desktop) {
+        int lDesk = desktop.getWidth();
+        int aDesk = desktop.getHeight();
+        int lIFrame = janela.getWidth();
+        int aIFrame = janela.getHeight();
+        janela.setLocation(lDesk / 2 - lIFrame / 2, aDesk / 2 - aIFrame / 2);
+        desktop.add(janela);
+        janela.setVisible(true);
+    }
     
+    public void abrirFormulario(JFrame janela, JDesktopPane desktop) {
+        int lDesk = desktop.getWidth();
+        int aDesk = desktop.getHeight();
+        int lIFrame = janela.getWidth();
+        int aIFrame = janela.getHeight();
+        janela.setLocation(lDesk / 2 - lIFrame / 2, aDesk / 2 - aIFrame / 2);
+        janela.setLocationRelativeTo(null);  
+        janela.setVisible(true);
+    }    
 }
--- AgeCon/src/util/Tabela.java
+++ AgeCon/src/util/Tabela.java
@@ -1,13 +1,74 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
-/**
- *
- * @author Fabrício
- */
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.RowSorter;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
 public class Tabela {
+
+    public JTable criarTabela(JPanel jpn, Object[] largura, Object[] pos, Object[] col) throws NullPointerException {
+        
+        JTable tabela = new JTable(new DefaultTableModel());
+        tabela.setVisible(true);
+        
+        DefaultTableModel modeloTabela = (DefaultTableModel) tabela.getModel();    
+
     
+        tabela.setFont(new Font("Arial", Font.BOLD, 16));
+        JScrollPane jsp = new JScrollPane(tabela);
+        tabela.setRowHeight(50);
+        jsp.setBounds(10, 40, 800, 350);
+        jsp.setVisible(true);
+        jpn.add(jsp);
+      
+
+        // Ordenando ao clicar no titulo da coluna da tabela
+        RowSorter<TableModel> sorter = new TableRowSorter<TableModel>(modeloTabela);
+        tabela.setRowSorter(sorter);             
+       
+        //Adicionando coluna
+        for (int i = 0; i < col.length; i++) {
+            modeloTabela.addColumn(col[i]);
+        }
+
+        //Criando objeto para alinhamento dos dados dentro da tabela
+        DefaultTableCellRenderer centro = new DefaultTableCellRenderer();
+        DefaultTableCellRenderer direita = new DefaultTableCellRenderer();
+        DefaultTableCellRenderer esquerda = new DefaultTableCellRenderer();
+
+        //Adicionando as posiçoes
+        centro.setHorizontalAlignment(SwingConstants.CENTER);
+        direita.setHorizontalAlignment(SwingConstants.RIGHT);
+        esquerda.setHorizontalAlignment(SwingConstants.LEFT);
+
+        //Trabalhando com as colunas 
+        for (int i = 0; i < largura.length; i++) {
+            if (pos[i].equals("centro")) {
+                pos[i] = centro;
+            }
+            if (pos[i].equals("direita")) {
+                pos[i] = direita;
+            }
+            if (pos[i].equals("esquerda")) {
+                pos[i] = esquerda;
+            }
+
+            tabela.getColumnModel().getColumn(i).setMaxWidth(
+                    Integer.parseInt(largura[i].toString())
+            );
+
+            tabela.getColumnModel().getColumn(i).setCellRenderer(
+                    (TableCellRenderer) pos[i]);
+
+        }
+        return tabela;
+    }
 }
