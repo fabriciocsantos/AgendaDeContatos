b9f3872710dfe1851519a58299be4b3f6fff32d8
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-01-30 15:44:47 -0300

    CidadeVisao

--- AgeCon/src/visao/CidadeVisao.java
+++ AgeCon/src/visao/CidadeVisao.java
@@ -63,11 +63,11 @@ public class CidadeVisao extends FormPadrao{
 
     @Override
     public void atualizarFormulario() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
 
     @Override
     public void excluirVisao() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
 }
