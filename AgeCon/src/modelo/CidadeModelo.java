eaa1f69585fb1fd7805d4158dd8f83e1efc3818c
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-02-01 14:25:01 -0300

    Criação
    Criação da classe modelo com getter e setter

--- AgeCon/src/modelo/CidadeModelo.java
+++ AgeCon/src/modelo/CidadeModelo.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Fabrício
- */
+
 public class CidadeModelo {
+    private int id;
+    private String descricao;
+    private String uf;
+    private String cep;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    
     
 }
--- AgeCon/src/modelo/ContatoModelo.java
+++ AgeCon/src/modelo/ContatoModelo.java
@@ -1,13 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Fabrício
- */
 public class ContatoModelo {
+
+    private int id;
+    private String Descricao;
+    private String Apelido;
+    private String Email;
+    private String Ddd;
+    private String Telefone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+
+    public String getApelido() {
+        return Apelido;
+    }
+
+    public void setApelido(String Apelido) {
+        this.Apelido = Apelido;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getDdd() {
+        return Ddd;
+    }
+
+    public void setDdd(String Ddd) {
+        this.Ddd = Ddd;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+    
     
 }
