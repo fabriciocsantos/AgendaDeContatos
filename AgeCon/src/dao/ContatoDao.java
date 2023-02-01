fb5b0958510404fd4b9a4a1af4b9b33d989282ce
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-02-01 15:22:44 -0300

    DAO
    Criação da classe dão para Contato, Cidade, Bairro

---
+++ AgeCon/src/dao/ContatoDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+import interfaces.InterfaceDao;
+import java.sql.SQLException;
+
+
+public class ContatoDao implements InterfaceDao {
+
+    @Override
+    public void salvarDao(Object... valor) {
+        
+    }
+
+    @Override
+    public void excluirDao(int id) {
+
+    }
+
+    @Override
+    public void consultarDao(Object... valor) throws SQLException {
+
+    }
+    
+}
